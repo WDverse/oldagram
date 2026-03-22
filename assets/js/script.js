6a76e33e911213a3f174405a838ee265fdca0d94
Emmanuel Appiagyei <emmanuelappiagyei@cmail.carleton.ca>
2026-03-22 14:02:17 -0400

    added feature to change  heart color and likes  when cliked

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,25 @@
 const heartIconEl = document.getElementById("heart-icon");
 const likesEl = document.getElementById("likes");
 
+//if the heart has not been clicked increase like by 1
+// if it has been clicked, remove like when clicked again
+
 const increaseLikeCount = () => {
-    let likes = parseInt((likesEl.textContent));
-    likes++
-    likesEl.textContent = likes
-}
+  let likes = parseInt(likesEl.textContent);
+  likesEl.classList.toggle("liked");
+  likesEl.classList.toggle("not-liked");
 
+  if (likesEl.classList.contains("not-liked")) {
+    likes--;
+    likesEl.textContent = likes;
+    heartIconEl.classList.remove("red-heart");
+  }
 
+  if (likesEl.classList.contains("liked")) {
+    likes++;
+    likesEl.textContent = likes;
+    heartIconEl.classList.add("red-heart");
+  }
+};
 
-heartIconEl.addEventListener("click", increaseLikeCount)
+heartIconEl.addEventListener("click", increaseLikeCount);
